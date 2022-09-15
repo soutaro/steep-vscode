a48243c56008d57b47930b2f6807f569941b40d8
Soutaro Matsumoto <matsumoto@soutaro.com>
2022-09-15 21:48:46 +0900

    Make LSP session management synchronized
    To reduce orphan Steep processes

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,48 @@
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, ExecutableOptions } from 'vscode-languageclient/node';
+import { LanguageClient, LanguageClientOptions, ServerOptions, ExecutableOptions, State } from 'vscode-languageclient/node';
 import { existsSync } from 'fs';
 
 const _clientSessions: Map<vscode.WorkspaceFolder, LanguageClient> = new Map();
 
-function stopSteep(folder: vscode.WorkspaceFolder) {
-	const client = _clientSessions.get(folder)
+async function waitFor(predicate: () => boolean, attempts: number, interval: number = 100) {
+	let count = 0
 
-	if (client) {
-		console.log(`Stopping steep in ${folder.uri}...`)
+	while (true) {
+		if (predicate()) {
+			return count
+		}
 
-		_clientSessions.delete(folder)
-		client.stop()
+		if (count > attempts) {
+			return false
+		}
+
+		await new Promise(s => setTimeout(s, interval))
+
+		count++
 	}
 }
 
-function hasClient(folder: vscode.WorkspaceFolder): boolean {
+async function stopSteep(folder: vscode.WorkspaceFolder) {
 	const client = _clientSessions.get(folder)
-	return client !== undefined
+
+	if (client) {
+		_clientSessions.delete(folder)
+
+		console.log(`Stopping steep in ${folder.uri}...`)
+
+		const wait = await waitFor(() => client.state !== State.Starting, 150)
+
+		if (!wait) {
+			console.error(`Failed to stop steep in ${folder.uri} 💩`)
+		} else {
+			await client.stop()
+		}
+
+		return client
+	}
 }
 
-function startSteep(folder: vscode.WorkspaceFolder) {
+async function startSteep(folder: vscode.WorkspaceFolder) {
 	if (_clientSessions.get(folder)) {
 		return
 	}
@@ -39,18 +61,19 @@ function startSteep(folder: vscode.WorkspaceFolder) {
 	let serverOptions: ServerOptions
 
 	const binstub = vscode.Uri.file(`${folder.uri.path}/bin/steep`)
+	const jobsOption = jobs ? [`--jobs=${jobs}`] : []
 	if (existsSync(binstub.fsPath)) {
 		console.log("Detected bin/steep...")
 		serverOptions = {
 			command: "bin/steep",
-			args: ["langserver", `--log-level=${loglevel}`, jobs ? `--jobs=${jobs}` : ''],
+			args: ["langserver", `--log-level=${loglevel}`, ...jobsOption],
 			options: options
 		}
 	} else {
 		console.log("Using bundle exec to start steep...")
 		serverOptions = {
 			command: "bundle",
-			args: ["exec", "steep", "langserver", `--log-level=${loglevel}`, jobs ? `--jobs=${jobs}` : ''],
+			args: ["exec", "steep", "langserver", `--log-level=${loglevel}`, ...jobsOption],
 			options: options
 		}
 	}
@@ -62,55 +85,56 @@ function startSteep(folder: vscode.WorkspaceFolder) {
 
 	const client = new LanguageClient("Steep", serverOptions, clientOptions)
 	_clientSessions.set(folder, client)
-	client.start()
+	await client.start()
 
 	vscode.window.setStatusBarMessage(`Started steep in ${folder.uri.fsPath}...`, 3000);
 }
 
-function ensureSteep() {
+async function ensureSteep() {
 	if (vscode.workspace.workspaceFolders) {
 		const folders: Set<vscode.WorkspaceFolder> = new Set(vscode.workspace.workspaceFolders)
-		_clientSessions.forEach((client, folder) => {
-			if (!folders.has(folder)) {
-				stopSteep(folder)
-			}
-		})
 
-		vscode.workspace.workspaceFolders.forEach(folder => {
+		for (const [folder, client] of _clientSessions) {
+			if (!folders.has(folder)) {
+				await stopSteep(folder)
+			}
+		}
+
+		for (const folder of vscode.workspace.workspaceFolders) {
 			console.log(`Checking if steep should start in ${folder.uri}...`)
 			if (folder.uri.scheme === "file") {
 				const file = vscode.Uri.file(`${folder.uri.path}/Steepfile`)
 				if (existsSync(file.fsPath)) {
-					startSteep(folder)
+					await startSteep(folder)
 				}
 			}
-		})
+		}
 	}
 }
 
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('steep.restartAll', () => {
-		vscode.workspace.workspaceFolders?.forEach((folder) => {
-			stopSteep(folder)
-			startSteep(folder)
+export async function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('steep.restartAll', () => {
+			vscode.workspace.workspaceFolders?.forEach(async (folder) => {
+				const client = _clientSessions.get(folder)
+				if (client) {
+					await client.restart()
+				}
+			})
 		})
-	});
+	)
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeWorkspaceFolders(async () => {
+			await ensureSteep()
+		})
+	)
 
-	vscode.workspace.onDidChangeWorkspaceFolders(() => {
-		ensureSteep()
-	})
-
-	ensureSteep()
+	await ensureSteep()
 }
 
 export function deactivate() {
-	_clientSessions.forEach((session, _) => {
-		session.stop()
+	_clientSessions.forEach(async (session, _) => {
+		await session.dispose()
 	})
-}
-
-export function restartAll() {
-	console.log(`restartAll`);
 }
